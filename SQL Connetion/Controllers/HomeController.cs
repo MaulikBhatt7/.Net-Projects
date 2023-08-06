e20724c836d4ec9caf454c129e48fc8c7ae433bf
Maulik Bhatt <101645973+MaulikBhatt7@users.noreply.github.com>
2023-08-06 21:07:30 +0530

    Add files via upload

---
+++ SQL Connetion/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Data_Annotation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Data_Annotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration Configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SQLView() {
+            SqlConnection sqlConnection = new SqlConnection(this.Configuration.GetConnectionString("default"));
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            sqlCommand.CommandText = "PR_Country_SelectAll";
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            DataTable dt = new DataTable();
+
+            dt.Load(sqlDataReader);
+            return View(dt);
+        }
+    }
+}
---
+++ SQL Connetion/Models/CityModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data_Annotation.Models
+{
+    public class CityModel
+    {
+        public int CityId { get; set; }
+
+        [Required]
+        public string CityName { get; set; }
+
+
+        public string? CityCode { get; set; }
+        
+    }
+}
